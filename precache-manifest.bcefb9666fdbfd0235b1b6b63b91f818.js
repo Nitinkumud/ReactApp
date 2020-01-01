461ab335da476eab07dd9b89c6ee3ab364aec862
unknown <nitin.kumar@primussoft.com-at-418809518040>
2020-01-01 23:00:35 +0530

    Updates

---
+++ precache-manifest.bcefb9666fdbfd0235b1b6b63b91f818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2480d1e9f8a8a62526c2cbeb76e631da",
+    "url": "/Nitinkumud/ReactApp/index.html"
+  },
+  {
+    "revision": "c2bb32b45f09016e3ae1",
+    "url": "/Nitinkumud/ReactApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/Nitinkumud/ReactApp/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Nitinkumud/ReactApp/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2bb32b45f09016e3ae1",
+    "url": "/Nitinkumud/ReactApp/static/js/main.e34f08b9.chunk.js"
+  },
+  {
+    "revision": "522dcb668a9b33f40e19",
+    "url": "/Nitinkumud/ReactApp/static/js/runtime-main.fff0c1b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Nitinkumud/ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
